1acc4125e21e8ed4fd436ac1457c9b1ec94f955b
Mostafa Yassien < Mostafa  GitHub>
2026-09-06 11:46:21 +0300

    Add project files.

---
+++ BookShoppingCartMvcUI/Migrations/20241101133144_editbook.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookShoppingCartMvcUI.Migrations
+{
+    /// <inheritdoc />
+    public partial class editbook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Book");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
---
+++ BookShoppingCartMvcUI/Models/Book.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookShoppingCartMvcUI.Models
+{
+    [Table("Book")]
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string? BookName { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string? AuthorName { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+        public List<OrderDetail> OrderDetail { get; set; }
+        public List<CartDetail> CartDetail { get; set; }
+        public Stock Stock { get; set; }
+
+        [NotMapped]
+        public string GenreName { get; set; }
+        [NotMapped]
+        public int Quantity { get; set; }
+
+
+    }
+}
